d4addf7ab363d93bc6a2c29b6460142570f979a3
dam <dam@203P18>
2024-02-22 10:07:31 +0100

    1er Commit Mauricio

--- src/paquete/EjercicioGIT.java
+++ src/paquete/EjercicioGIT.java
@@ -5,6 +5,7 @@ public class EjercicioGIT
     public static void main(String[] args) 
     {
         System.out.println("EjercioGit: Soy Mauricio");
+        System.out.println("EjercioGit: 1");
     }
     
 }
