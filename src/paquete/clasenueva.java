926fa6fb449a8c73345ba90bb0056e1cb18713a1
dam <dam@203P18>
2024-02-22 10:11:24 +0100

    2do Commit Mauricio(clase nueva)

--- src/paquete/clasenueva.java
+++ src/paquete/clasenueva.java
@@ -1,5 +1,9 @@
 package paquete;
 
-public class clasenueva {
-    
+public class clasenueva 
+{
+    public static void main( String[] args)
+    {
+        System.out.println("En la rama. Se crea la clase nueva");
+    }
 }
